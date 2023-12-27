132df8c9ee9ca4984e25e1afb5c53fe73605462c
Fu Wang <wangfu91@hotmail.com>
2023-12-27 18:29:43 +0800

    Save in progress work.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::mem::size_of;
+use std::{ffi::OsString, mem::size_of, os::windows::ffi::OsStringExt};
 
 use windows::{
-    Wdk::System::SystemInformation::{NtQuerySystemInformation, SYSTEM_INFORMATION_CLASS},
+    Wdk::{
+        Foundation::{
+            NtQueryObject, ObjectTypeInformation, OBJECT_INFORMATION_CLASS, OBJECT_NAME_INFORMATION,
+        },
+        System::SystemInformation::{NtQuerySystemInformation, SYSTEM_INFORMATION_CLASS},
+    },
     Win32::{
-        Foundation::{FALSE, STATUS_INFO_LENGTH_MISMATCH},
-        System::Threading::{OpenProcess, PROCESS_DUP_HANDLE},
+        Foundation::{
+            CloseHandle, DuplicateHandle, DUPLICATE_SAME_ACCESS, FALSE, HANDLE,
+            STATUS_INFO_LENGTH_MISMATCH, UNICODE_STRING,
+        },
+        Storage::FileSystem::{GetFileType, FILE_TYPE_DISK},
+        System::{
+            Threading::{GetCurrentProcess, OpenProcess, PROCESS_DUP_HANDLE},
+            WindowsProgramming::PUBLIC_OBJECT_TYPE_INFORMATION,
+        },
     },
 };
 
@@ -28,7 +40,7 @@ fn main() {
         println!("nt_status: {:?}", nt_status);
 
         if nt_status == STATUS_INFO_LENGTH_MISMATCH {
-            println!("STATUS_INFO_LENGTH_MISMATCH, double the buffer size and try again");
+            // STATUS_INFO_LENGTH_MISMATCH, increase the buffer size and try again.
             buffer.resize(return_len as usize, 0);
             continue;
         }
@@ -44,24 +56,25 @@ fn main() {
         break;
     }
 
-    let handle_info = unsafe { &*(buffer.as_ptr() as *const SYSTEM_HANDLE_INFORMATION_EX) };
+    let handle_info = unsafe { &*(buffer.as_ptr() as *const SystemHandleInformationEx) };
 
-    let handle_count = handle_info.NumberOfHandles;
+    let handle_count = handle_info.number_of_handles;
     println!("handle_count: {}", handle_count);
 
     for i in 0..handle_count {
-        let handle =
-            unsafe {
-                std::ptr::read(
-                    handle_info.Handles.as_ptr().offset(
-                        (i * size_of::<SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX>() as u32) as isize,
-                    ) as *const SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX,
-                )
-            };
+        let offset = i * size_of::<SystemHandleTableEntryInfoEx>();
+        if offset > return_len as usize {
+            break;
+        }
 
-        println!("handle: {:?}", handle);
-        let pid = handle.UniqueProcessId;
-        println!("pid: {}", pid);
+        let handle: SystemHandleTableEntryInfoEx = unsafe {
+            std::ptr::read(handle_info.handles.as_ptr().offset(offset as isize)
+                as *const SystemHandleTableEntryInfoEx)
+        };
+
+        //println!("handle: {:?}", handle);
+        let pid = handle.unique_process_id;
+        //println!("pid: {}", pid);
 
         if pid == 0 {
             continue;
@@ -69,29 +82,129 @@ fn main() {
 
         // https://stackoverflow.com/questions/46384048/enumerate-handles
 
-        let process_handle = unsafe { OpenProcess(PROCESS_DUP_HANDLE, FALSE, pid) };
+        let process_handle = unsafe { OpenProcess(PROCESS_DUP_HANDLE, FALSE, pid as u32) };
 
-        //
+        if let Err(e) = process_handle {
+            //println!("OpenProcess failed, e: {:?}", e);
+            continue;
+        }
+
+        let process_handle = process_handle.unwrap();
+
+        let mut dup_handle = HANDLE::default();
+
+        let dup_handle_result = unsafe {
+            DuplicateHandle(
+                process_handle,
+                HANDLE(handle.handle_value as isize),
+                GetCurrentProcess(),
+                &mut dup_handle,
+                0,
+                FALSE,
+                DUPLICATE_SAME_ACCESS,
+            )
+        };
+
+        if let Err(e) = dup_handle_result {
+            //println!("DuplicateHandle failed, e: {:?}", e);
+            continue;
+        }
+
+        let mut return_length = 0u32;
+        let mut object_type_info = vec![0u8; 1024];
+        let nt_status = unsafe {
+            NtQueryObject(
+                dup_handle,
+                ObjectTypeInformation,
+                Some(object_type_info.as_mut_ptr() as *mut _),
+                object_type_info.len() as u32,
+                Some(&mut return_length),
+            )
+        };
+
+        if nt_status.is_err() {
+            println!("NtQueryObject failed, nt_status: {:?}", nt_status);
+            let _ = unsafe { CloseHandle(dup_handle) };
+            continue;
+        }
+
+        let object_type_info =
+            unsafe { &*(object_type_info.as_ptr() as *const PUBLIC_OBJECT_TYPE_INFORMATION) };
+
+        let slice = unsafe {
+            std::slice::from_raw_parts(
+                object_type_info.TypeName.Buffer.as_ptr(),
+                object_type_info.TypeName.Length as usize / std::mem::size_of::<u16>(),
+            )
+        };
+
+        let os_string = OsString::from_wide(slice);
+        let object_type_name = os_string.to_string_lossy().to_string();
+        //println!("pid: {}", pid);
+        //println!("object_type_name: {}", object_type_name);
+
+        let file_type = unsafe { GetFileType(dup_handle) };
+        //println!("file_type: {:?}", file_type);
+        if file_type != FILE_TYPE_DISK {
+            let _ = unsafe { CloseHandle(dup_handle) };
+            continue;
+        }
+
+        let ObjectNameInformation = OBJECT_INFORMATION_CLASS(1);
+        let mut return_length = 0u32;
+        let mut buffer = vec![0u8; 1024];
+
+        let nt_status = unsafe {
+            NtQueryObject(
+                dup_handle,
+                ObjectNameInformation,
+                Some(buffer.as_mut_ptr() as *mut _),
+                buffer.len() as u32,
+                Some(&mut return_length),
+            )
+        };
+
+        if nt_status.is_err() {
+            println!("NtQueryObject failed, nt_status: {:?}", nt_status);
+            let _ = unsafe { CloseHandle(dup_handle) };
+            continue;
+        }
+
+        let object_name_info = unsafe { &*(buffer.as_ptr() as *const OBJECT_NAME_INFORMATION) };
+
+        let slice = unsafe {
+            std::slice::from_raw_parts(
+                object_name_info.Name.Buffer.as_ptr(),
+                object_name_info.Name.Length as usize / std::mem::size_of::<u16>(),
+            )
+        };
+        let os_string = OsString::from_wide(slice);
+        let object_name = os_string.to_string_lossy().to_string();
+        println!("pid = {}, object_name: {}", pid, object_name);
+
+        let _ = unsafe { CloseHandle(dup_handle) };
     }
 
     println!("handle_info: {:?}", handle_info);
 }
 
+#[repr(C)]
 #[derive(Debug)]
-pub struct SYSTEM_HANDLE_INFORMATION_EX {
-    pub NumberOfHandles: u32,
-    pub Reserved: u32,
-    pub Handles: [SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX; 1],
+pub struct SystemHandleInformationEx {
+    number_of_handles: usize,
+    reserved: usize,
+    handles: [SystemHandleTableEntryInfoEx; 1],
 }
 
+#[repr(C)]
 #[derive(Debug)]
-pub struct SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX {
-    pub Object: *mut ::core::ffi::c_void,
-    pub UniqueProcessId: u32,
-    pub HandleValue: u32,
-    pub GrantedAccess: u32,
-    pub CreatorBackTraceIndex: u16,
-    pub ObjectTypeIndex: u16,
-    pub HandleAttributes: u32,
-    pub Reserved: u32,
+pub struct SystemHandleTableEntryInfoEx {
+    object: *mut ::core::ffi::c_void,
+    unique_process_id: usize,
+    handle_value: usize,
+    granted_access: u32,
+    creator_back_trace_index: u16,
+    object_type_index: u16,
+    handle_attributes: u32,
+    reserved: u32,
 }
